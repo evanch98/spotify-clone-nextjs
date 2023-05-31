54971f9406f9271c21f970c00b4d2d2468d7ce60
Kyaw Thu <evanch98@gmail.com>
2023-05-31 23:52:07 +0800

    add the Link component and reformat the code

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,18 +1,31 @@
 "use client";
 
+import Link from "next/link";
 import { IconType } from "react-icons";
+import { twMerge } from "tailwind-merge";
 
 interface SidebarItemProps {
-  icon: IconType;
-  label: string;
-  active: boolean;
-  href: string;
+	icon: IconType;
+	label: string;
+	active: boolean;
+	href: string;
 }
 
-const SidebarItem: React.FC<SidebarItemProps> = ({ icon, label, active, href }) => {
-  return ( 
-    <div></div>
-  );
-}
+const SidebarItem: React.FC<SidebarItemProps> = ({
+	icon,
+	label,
+	active,
+	href,
+}) => {
+	return (
+		<Link
+			href={href}
+			className={twMerge(
+				"flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1",
+				active && "text-white"
+			)}
+		></Link>
+	);
+};
 
 export default SidebarItem;
