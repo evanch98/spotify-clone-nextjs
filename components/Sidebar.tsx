42205fece0e725971eb526ae925cb4ab6b8ddfd6
Kyaw Thu <evanch98@gmail.com>
2023-05-31 23:40:18 +0800

    reformat the code

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,38 +7,37 @@ import { BiSearch } from "react-icons/bi";
 import Box from "./Box";
 
 interface SidebarProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({
-  children
-}) => {
-  const pathname = usePathname();
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+	const pathname = usePathname();
 
-  const routes = useMemo(() => [
-    {
-      icon: HiHome,
-      label: "Home",
-      active: pathname !== "/search",
-      href: "/",
-    },
-    {
-      icon: BiSearch,
-      label: "Search",
-      active: pathname === "/search",
-      href: "/search"
-    }
-  ], [pathname]);
+	const routes = useMemo(
+		() => [
+			{
+				icon: HiHome,
+				label: "Home",
+				active: pathname !== "/search",
+				href: "/",
+			},
+			{
+				icon: BiSearch,
+				label: "Search",
+				active: pathname === "/search",
+				href: "/search",
+			},
+		],
+		[pathname]
+	);
 
-  return ( 
-    <div className="flex h-full">
-      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
-        <Box>
-          Sidebar Navigation
-        </Box>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex h-full">
+			<div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+				<Box>Sidebar Navigation</Box>
+			</div>
+		</div>
+	);
 };
 
 export default Sidebar;
