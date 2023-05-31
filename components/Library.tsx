5d109b86a0bbd8f37bc42e87659fc976880bf394
Kyaw Thu <evanch98@gmail.com>
2023-06-01 00:14:27 +0800

    fix typo

--- components/Library.tsx
+++ components/Library.tsx
@@ -11,7 +11,7 @@ const Library = () => {
       <div className="flex items-center justify-between px-5 pt-4">
         <div className="inline-flex items-center gap-x-2">
           <TbPlaylist size={26} className="text-neutral-400" />
-          <p className="text-neutral-400 font-medium text-md">Your Library</p>
+          <p className="text-neutral-400 font-medium text-base">Your Library</p>
         </div>
       </div>
     </div>
