2edde1725b4b47b93936d796848f0e157d5cecd9
Kyaw Thu <evanch98@gmail.com>
2023-06-01 00:13:39 +0800

    update the Library component

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,8 +1,20 @@
 "use client";
 
+import { TbPlaylist } from "react-icons/tb";
+
 const Library = () => {
+  const onClick = () => {
+    // Handle upload later
+  };
   return ( 
-    <div>Library</div>
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist size={26} className="text-neutral-400" />
+          <p className="text-neutral-400 font-medium text-md">Your Library</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
