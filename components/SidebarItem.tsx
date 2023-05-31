c837e08b3de5bd7d31fb7d0ee0729a97edda5966
Kyaw Thu <evanch98@gmail.com>
2023-06-01 00:14:48 +0800

    fix typo

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -21,7 +21,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
     <Link
       href={href}
       className={twMerge(
-        "flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1",
+        "flex flex-row h-auto items-center w-full gap-x-4 text-base font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1",
         active && "text-white"
       )}
     >
