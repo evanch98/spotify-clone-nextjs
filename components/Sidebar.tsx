e3a6f80410c0125b8bbe3e9af77268ad6f28e741
Kyaw Thu <evanch98@gmail.com>
2023-05-31 23:29:59 +0800

    import the Box component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
 
 interface SidebarProps {
   children: React.ReactNode;
