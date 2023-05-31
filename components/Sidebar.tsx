f6bb59a0e8dea92d17ed9c198880ec0c1830cf9f
Kyaw Thu <evanch98@gmail.com>
2023-05-31 23:47:08 +0800

    reformat the code

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -36,15 +36,12 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 		<div className="flex h-full">
 			<div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
 				<Box>
-          <div className="flex flex-col gap-y-4 px-5 py-4">
-            {routes.map((item) => (
-              <SidebarItem
-                key={item.label}
-                {...item}
-              />
-            ))}
-          </div>
-        </Box>
+					<div className="flex flex-col gap-y-4 px-5 py-4">
+						{routes.map((item) => (
+							<SidebarItem key={item.label} {...item} />
+						))}
+					</div>
+				</Box>
 				<Box>Sound Library</Box>
 			</div>
 		</div>
