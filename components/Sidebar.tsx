a317fe5120fef6f47bdda6fec7aff50b8f52eab2
Kyaw Thu <evanch98@gmail.com>
2023-06-01 00:01:29 +0800

    add the children

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -44,6 +44,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 				</Box>
 				<Box>Sound Library</Box>
 			</div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
 		</div>
 	);
 };
