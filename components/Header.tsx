53df9d2c240d6c5fe1f354fafebeea682d779fd4
Kyaw Thu <evanch98@gmail.com>
2023-06-01 00:27:56 +0800

    add router and handleLogout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // Handle log out
+  };
+
   return ( 
     <div>{children}</div>
   );
