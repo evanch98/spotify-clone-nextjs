e1005d4b0950ba2b85040585bf26aba6a6b49ec8
Kyaw Thu <evanch98@gmail.com>
2023-06-01 00:23:38 +0800

    style the div and add Header component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,10 @@
 
 export default function Home() {
   return (
-    <div className="text-green-500">Hello (Site)!</div>
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        Header
+      </Header>
+    </div>
   );
 };
