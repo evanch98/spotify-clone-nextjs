92c1b897f578a5a15ddf00506ed7f5b31f97d45a
Kyaw Thu <evanch98@gmail.com>
2023-06-01 13:12:46 +0800

    add ListItem component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
@@ -6,7 +7,9 @@ export default function Home() {
       <Header>
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4"></div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem />
+          </div>
         </div>
       </Header>
     </div>
