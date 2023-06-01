b9dc9c6f2afd923a5d37097526779868780aaacc
Kyaw Thu <evanch98@gmail.com>
2023-06-01 13:38:18 +0800

    add onClick props

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -19,7 +19,10 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
   };
 
   return (
-    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+    <button
+      onClick={onClick}
+      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+    >
       <div className="relative min-h-[64px] min-w-[64px]">
         <Image className="object-cover" fill src={image} alt="Image" />
       </div>
