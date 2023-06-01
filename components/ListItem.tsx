9a36d22355fb3bef0416da1c2c743defe5c62c0c
Kyaw Thu <evanch98@gmail.com>
2023-06-01 13:37:21 +0800

    add the play button

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
   image: string;
@@ -23,6 +24,9 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
         <Image className="object-cover" fill src={image} alt="Image" />
       </div>
       <p className="font-medium truncate py-5">{name}</p>
+      <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+        <FaPlay className="text-black" />
+      </div>
     </button>
   );
 };
