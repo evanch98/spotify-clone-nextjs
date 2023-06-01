cb1f476469f9bd28a5d0cdedafe38140924dfbc0
Kyaw Thu <evanch98@gmail.com>
2023-06-01 13:22:54 +0800

    add the image source

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem />
+            <ListItem 
+              image="/images/liked.png"
+            />
           </div>
         </div>
       </Header>
