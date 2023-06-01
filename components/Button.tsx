0117e2d61e934ed363883a90b86817e2dd98e1bb
Kyaw Thu <evanch98@gmail.com>
2023-06-01 13:04:22 +0800

    update the Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,15 +1,29 @@
 "use client";
 
-interface ButtonProps {
-  children: React.ReactNode;
-}
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
-  return ( 
-    <div>
-      {children}
-    </div>
-  );
-}
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        type={type}
+        className={twMerge(
+          "w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition",
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
 
 export default Button;
