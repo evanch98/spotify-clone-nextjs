f7e4ba0c7167a16da726208fcf810c9fde515992
Kyaw Thu <evanch98@gmail.com>
2023-06-01 13:34:37 +0800

    remove the outer div

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -18,14 +18,12 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
   };
 
   return (
-    <div>
-      <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
-        <div className="relative min-h-[64px] min-w-[64px]">
-          <Image className="object-cover" fill src={image} alt="Image" />
-        </div>
-        <p>{name}</p>
-      </button>
-    </div>
+    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+      <div className="relative min-h-[64px] min-w-[64px]">
+        <Image className="object-cover" fill src={image} alt="Image" />
+      </div>
+      <p className="font-medium truncate py-5">{name}</p>
+    </button>
   );
 };
 
