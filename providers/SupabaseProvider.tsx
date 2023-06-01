8de3601edbce816f4fd6df8bb00498567d49221f
Kyaw Thu <evanch98@gmail.com>
2023-06-01 19:27:35 +0800

    add a new client component SupabaseProvider

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Database } from "@/types_db";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {};
