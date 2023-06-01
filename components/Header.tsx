dafa55b623f1c4c0beefbb414fc205d3001a551f
Kyaw Thu <evanch98@gmail.com>
2023-06-01 13:07:10 +0800

    add empty onclick

--- components/Header.tsx
+++ components/Header.tsx
@@ -52,12 +52,15 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         <div className="flex justify-between items-center gap-x-4">
           <>
             <div>
-              <Button className="bg-transparent text-neutral-300 font-medium">
+              <Button
+                onClick={() => {}}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
                 Sign up
               </Button>
             </div>
             <div>
-              <Button className="bg-white px-6 py-2">
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
                 Log in
               </Button>
             </div>
