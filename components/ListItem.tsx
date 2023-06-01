34fd0ccc462aa7795ca7dd520e94374ff57810ff
Kyaw Thu <evanch98@gmail.com>
2023-06-01 13:25:51 +0800

    reformat the code

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -15,17 +15,17 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
   const onClick = () => {
     // Add authentication before push
     router.push(href);
-  }
+  };
 
-  return ( 
+  return (
     <div>
       <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
-        <div className="relative min-h-[64px] min-2-[64px]">
-          <Image />
+        <div className="relative min-h-[64px] min-w-[64px]">
+          <Image className="object-cover" fill src={image} alt="Image" />
         </div>
       </button>
     </div>
   );
-}
+};
 
 export default ListItem;
