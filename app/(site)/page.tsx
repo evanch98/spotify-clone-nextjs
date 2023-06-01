e030f3a701fc20f8b4a3b15863f2112a1de361bd
Kyaw Thu <evanch98@gmail.com>
2023-06-01 13:39:57 +0800

    add another title

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem 
+            <ListItem
               image="/images/liked.png"
               name="Liked Songs"
               href="liked"
@@ -16,6 +16,11 @@ export default function Home() {
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
+        </div>
+      </div>
     </div>
   );
 }
