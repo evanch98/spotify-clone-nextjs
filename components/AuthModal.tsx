f09303b8b211a5f6128dd1b933f4a55b5b7d6ac4
Kyaw Thu <evanch98@gmail.com>
2023-06-02 22:01:30 +0800

    add contents to the AuthModal component

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,18 +1,44 @@
 "use client";
 
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
 import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 const AuthModal = () => {
-  return ( 
+  const supabaseClient = useSupabaseClient();
+  const router = useRouter();
+  const { session } = useSessionContext();
+  return (
     <Modal
       title="Welcome back"
       description="Login to your account"
       isOpen
       onChange={() => {}}
     >
-      Auth modal children!
+      <Auth 
+        theme="dark"
+        magicLink
+        providers={["github"]}
+        supabaseClient={supabaseClient}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#22c55e"
+              }
+            }
+          }
+        }}
+      />
     </Modal>
   );
-}
+};
 
 export default AuthModal;
