26f071117136cfcfb55555567b62d6bf3a923fb5
Kyaw Thu <evanch98@gmail.com>
2023-06-02 20:11:43 +0800

    add new parameters

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,11 +1,24 @@
 "use client";
 
-const Modal = () => {
-  return ( 
-    <div>
-      Modal Component
-    </div>
-  );
+import * as Dialog from "@radix-ui/react-dialog";
+import { IoMdClose } from "react-icons/io";
+
+interface ModalProps {
+  isOpen: boolean;
+  onChange: (open: boolean) => void;
+  title: string;
+  description: string;
+  children: React.ReactNode;
 }
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onChange,
+  title,
+  description,
+  children,
+}) => {
+  return <div>Modal Component</div>;
+};
 
 export default Modal;
