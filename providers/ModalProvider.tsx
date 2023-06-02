ea3422c0b6b9f0b58cd7d144d78124a9873a715b
Kyaw Thu <evanch98@gmail.com>
2023-06-02 19:50:47 +0800

    add a logic to ensure that none of the modals can be seen or opened during the server-side rendering

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,19 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // ensuring that none of the modals can be seen or opened during the server-side rendering
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return ( 
     <>
       Modals
