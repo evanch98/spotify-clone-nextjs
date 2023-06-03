c960680a7676d05e885b73c7222dfc43b54d7229
Kyaw Thu <evanch98@gmail.com>
2023-06-03 13:12:59 +0800

    update the UI of the header when the user has logged in

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -65,7 +66,14 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         </div>
         <div className="flex justify-between items-center gap-x-4">
           {user ? (
-            <div>Logged in</div>
+            <div className="flex gap-x-4 items-center">
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                Logout
+              </Button>
+              <Button onClick={() => router.push("/account")} className="bg-white">
+                <FaUserAlt />
+              </Button>
+            </div>
           ) : (
             <>
               <div>
