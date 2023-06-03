b3392e928b5c5202e15793000f6b3507c7ab5ba3
Kyaw Thu <evanch98@gmail.com>
2023-06-03 12:50:09 +0800

    add a logic to open the AuthModal

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -8,17 +8,26 @@ import Modal from "./Modal";
 import { useRouter } from "next/navigation";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import useAuthModal from "@/hooks/useAuthModal";
 
 const AuthModal = () => {
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
   const { session } = useSessionContext();
+  const { onClose, isOpen } = useAuthModal();
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  }
+
   return (
     <Modal
       title="Welcome back"
       description="Login to your account"
-      isOpen
-      onChange={() => {}}
+      isOpen={isOpen}
+      onChange={onChange}
     >
       <Auth 
         theme="dark"
