7b4e29e3b9758568dce861d08d184fc1af86d494
Kyaw Thu <evanch98@gmail.com>
2023-06-17 00:51:05 +0800

    update the Library component

--- components/Library.tsx
+++ components/Library.tsx
@@ -4,15 +4,21 @@ import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const Library = () => {
   const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
   const { user } = useUser();
   const onClick = () => {
     if (!user) {
       authModal.onOpen();
     }
-    // Handle upload
+
+    // TODO: Check for subscription
+
+    // Handle opening upload modal
+    return uploadModal.onOpen();
   };
   return (
     <div className="flex flex-col">
