dbb59be5ce36886e6c230faf298a6548747cadac
Kyaw Thu <evanch98@gmail.com>
2023-06-20 22:07:54 +0800

    add a custom onChange function

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -5,12 +5,19 @@ import Modal from "./Modal";
 
 const UploadModal = () => {
   const uploadModal = useUploadModal();
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      // Reset the form
+      uploadModal.onClose();
+    }
+  }
   return (
     <Modal
       title="Upload modal title"
       description="Upload modal description"
-      isOpen
-      onChange={() => {}}
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
     >
       Upload Modal
     </Modal>
