b1c7bacff4dc2a10c25786a03719c228eb3e3066
Kyaw Thu <evanch98@gmail.com>
2023-06-24 23:00:55 +0800

    add the Input component that is not defined yet

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -3,8 +3,10 @@
 import useUploadModal from "@/hooks/useUploadModal";
 import Modal from "./Modal";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
 
 const UploadModal = () => {
+  const [isLoading, setIsLoading] = useState();
   const uploadModal = useUploadModal();
 
   const { register, handleSubmit, reset } = useForm<FieldValues>({
@@ -38,7 +40,14 @@ const UploadModal = () => {
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      <form onSubmit={handleSubmit(onSubmit)}></form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song title"
+        />
+      </form>
     </Modal>
   );
 };
