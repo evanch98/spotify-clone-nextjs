12581b0c7372d6a14261d6b40a7db8a5db0a6e49
Kyaw Thu <evanch98@gmail.com>
2023-06-24 23:12:28 +0800

    update the Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,7 +1,28 @@
 "use client";
 
-const Input = () => {
-  return <div>Input</div>;
-};
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+// reuse the React.HTMLAttributes props so that we don't need to rewrite all the props
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, disabled, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={twMerge(
+          "flex w-full rounded-md bg-neutral-700 border border-transparent px-3 py-3 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-400 disabled:cursor-not-allowed disabled:opacity-50 focus:outline-none",
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
 
 export default Input;
