8449daa8d23b70c81e1906a4560da82ad29337e4
Kyaw Thu <evanch98@gmail.com>
2023-06-27 12:24:34 +0800

    update the SongItem component

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -24,6 +24,12 @@ const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
           alt="Cover Image"
         />
       </div>
+      <div className="flex flex-col items-center w-full p-4 gap-y-1">
+        <h1 className="font-semibold truncate w-full">{data.title}</h1>
+        <p className="text-neutral-400 text-sm pb-4 w-full truncate">
+          {data.author}
+        </p>
+      </div>
     </div>
   );
 };
