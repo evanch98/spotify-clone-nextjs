4aba1b93b690bcf04aac07ed2d98651d24bb9cc4
Kyaw Thu <evanch98@gmail.com>
2023-06-27 12:14:57 +0800

    update the SongItem component

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
+import Image from "next/image";
 
 interface SongItemProps {
   data: Song;
@@ -8,7 +10,20 @@ interface SongItemProps {
 }
 
 const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
-  return <div>SongItem</div>;
+  const imagePath = useLoadImage(data); // retrieve the path of the image of the given song
+  return (
+    <div
+      onClick={() => onClick(data.id)}
+      className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition p-3"
+    >
+      <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
+        <Image
+          className="object-cover"
+          src={imagePath || "/images/liked.png"}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SongItem;
