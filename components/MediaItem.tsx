fcc8b6b4b373ec9b352e80e5f50fe340040cbdf8
Kyaw Thu <evanch98@gmail.com>
2023-06-27 18:42:23 +0800

    update the MediaItem component

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -2,6 +2,7 @@
 
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
+import Image from "next/image";
 
 interface MediaItemProps {
   data: Song;
@@ -20,7 +21,25 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
 
     // Default turn on player
   };
-  return <div>MediaItem</div>;
+  return (
+    <div
+      onClick={handleClick}
+      className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md"
+    >
+      <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
+        <Image
+          fill
+          src={imagePath || "/images/liked.png"}
+          alt="Cover Image"
+          className="object-cover"
+        />
+      </div>
+      <div className="flex flex-col gap-y-1 overflow-hidden">
+        <h1 className="text-white truncate">{data.title}</h1>
+        <p className="text-neutral-400 text-sm truncate">{data.author}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MediaItem;
