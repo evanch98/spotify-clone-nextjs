2bb5cca52e2abcae58715150eff00e9359d91fb0
Kyaw Thu <evanch98@gmail.com>
2023-06-27 11:58:32 +0800

    add the SongItem component

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import SongItem from "@/components/SongItem";
 import { Song } from "@/types";
 
 interface PageContentProps {
@@ -14,7 +15,7 @@ const PageContent: React.FC<PageContentProps> = ({ songs }) => {
   // otherwise,
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
-      Page Content
+      <SongItem />
     </div>
   );
 };
