4eb02d1938c718e3088ac25c1dbb2db35604b948
Kyaw Thu <evanch98@gmail.com>
2023-06-27 13:36:26 +0800

    update the MediaItem component

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
 
 interface MediaItemProps {
@@ -8,6 +9,17 @@ interface MediaItemProps {
 }
 
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+  const imagePath = useLoadImage(data); // retrieve the path of the image of the given song
+
+  // handle the onClick
+  const handleClick = () => {
+    // if the onClick function exists
+    if (onClick) {
+      return onClick(data.id);
+    }
+
+    // Default turn on player
+  };
   return <div>MediaItem</div>;
 };
 
