9a8c982afe48cd7d23ca96f308edb8aace937fac
Kyaw Thu <evanch98@gmail.com>
2023-06-27 12:19:35 +0800

    update the image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["zjrmnpipkcxsatrlqdfg.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
