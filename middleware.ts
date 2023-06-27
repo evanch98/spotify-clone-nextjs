1a309c07e8848a454098e5689290dc66221ff4ec
Kyaw Thu <evanch98@gmail.com>
2023-06-27 13:15:27 +0800

    add comments

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextRequest, NextResponse } from "next/server";
 
+// to run code before a request is completed
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({
