9223131cd5b3f106779ac9f6d0e36cd2cdd50971
Kyaw Thu <evanch98@gmail.com>
2023-06-27 13:33:36 +0800

    add new props

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-const MediaItem = () => {
+import { Song } from "@/types";
+
+interface MediaItemProps {
+  data: Song;
+  onClick?: (id: string) => void;
+}
+
+const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
   return <div>MediaItem</div>;
 };
 
