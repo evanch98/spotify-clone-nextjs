f33f9375d6831924fbb9c050ba6be7fad8385294
Kyaw Thu <evanch98@gmail.com>
2023-06-27 12:06:48 +0800

    initialize the useLoadingImage function

---
+++ hooks/useLoadImage.ts
@@ -0,0 +1,17 @@
+import { Song } from "@/types";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+
+// a hook for loading an image
+const useLoadImage = (song: Song) => {
+  const supabaseClient = useSupabaseClient();
+
+  // if there is no song, return null
+  if (!song) {
+    return null;
+  }
+
+  // fetch the image of the given path from the supabase storage bucket (images)
+  const { data: imageData } = supabaseClient.storage
+    .from("images")
+    .getPublicUrl(song.image_path);
+};
