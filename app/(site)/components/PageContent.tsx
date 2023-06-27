4dab31de4f1e3d8fb5494ba2d956ee2e551fb42b
Kyaw Thu <evanch98@gmail.com>
2023-06-27 12:02:20 +0800

    pass data to the SongItem's props

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -16,7 +16,7 @@ const PageContent: React.FC<PageContentProps> = ({ songs }) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
       {songs.map((song) => (
-        <SongItem key={song.id} />
+        <SongItem key={song.id} data={song} onClick={() => {}} />
       ))}
     </div>
   );
