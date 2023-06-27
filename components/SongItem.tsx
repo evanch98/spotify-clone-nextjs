c1fa66472f79bd2c4092d75c2108c90f24dac340
Kyaw Thu <evanch98@gmail.com>
2023-06-27 12:29:38 +0800

    add the PlayButton component

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -3,6 +3,7 @@
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
 import Image from "next/image";
+import PlayButton from "./PlayButton";
 
 interface SongItemProps {
   data: Song;
@@ -31,7 +32,7 @@ const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
         </p>
       </div>
       <div className="absolute bottom-24 right-5">
-        Play Button
+        <PlayButton />
       </div>
     </div>
   );
