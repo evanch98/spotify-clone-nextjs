1d6d93b162ad69379e6d4f376472aae9e8871820
Kyaw Thu <evanch98@gmail.com>
2023-06-28 18:55:36 +0800

    add the SearchContent component

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
 
 interface SearchProps {
   searchParams: {
@@ -20,6 +21,7 @@ const Search = async ({ searchParams }: SearchProps) => {
           <SearchInput />
         </div>
       </Header>
+      <SearchContent songs={songs} />
     </div>
   );
 };
