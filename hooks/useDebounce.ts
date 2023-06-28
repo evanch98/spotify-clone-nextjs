564bf13c143cffdd19f83bab27db97d9c4028f70
Kyaw Thu <evanch98@gmail.com>
2023-06-28 17:49:20 +0800

    add a new function useDebounce

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+// to avoid refetching songs on every single input by adding a delay
+// will only refetch the song when user stops typing for a certain amount of time
+function useDebounce<T>(value: T, delay?: number): T {
+  const [debounceValue, setDebounceValue] = useState<T>(value);
+
+  // will only get the result after 500 milliseconds (if the delayed is not specified) of user not typing
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay || 500);
+
+    return () => {
+      // clear the timer
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debounceValue;
+}
+
+export default useDebounce;
