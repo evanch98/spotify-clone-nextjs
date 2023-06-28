d82b390ae980f8810f58e44b2f73d1a99f1c1c3c
Kyaw Thu <evanch98@gmail.com>
2023-06-29 01:00:01 +0800

    update the Liked component

--- app/liked/page.tsx
+++ app/liked/page.tsx
@@ -1,7 +1,35 @@
+import getLikedSongs from "@/actions/getLikedSongs";
+import Header from "@/components/Header";
+import Image from "next/image";
+
 export const revalidate = 0; // to make sure this page is not cached and the data on it will always be up to date
 
-const Liked = () => {
-  return <div>Liked Songs</div>;
+const Liked = async () => {
+  const songs = await getLikedSongs();
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mt-20">
+          <div className="flex flex-col md:flex-row items-center gap-x-5">
+            <div className="relative h-32 w-32 lg:h-44 lg:w-44">
+              <Image
+                fill
+                src="/images/Liked.png"
+                alt="Playlist"
+                className="object-cover"
+              />
+            </div>
+            <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
+              <p className="hidden md:block font-semibold text-sm">Playlist</p>
+              <h1 className="text-white text-4xl sm:text-5xl lg:text-7xl font-bold">
+                Liked Songs
+              </h1>
+            </div>
+          </div>
+        </div>
+      </Header>
+    </div>
+  );
 };
 
 export default Liked;
