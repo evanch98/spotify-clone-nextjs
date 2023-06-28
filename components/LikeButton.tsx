d4b069dc5f70115f348623d3933fd6c3223dc4bf
Kyaw Thu <evanch98@gmail.com>
2023-06-28 19:40:33 +0800

    update the LikeButton component

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -5,6 +5,8 @@ import { useUser } from "@/hooks/useUser";
 import { useSessionContext } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 interface LikeButtonProps {
   songId: string;
@@ -32,6 +34,7 @@ const LikeButton: React.FC<LikeButtonProps> = ({ songId }) => {
         .eq("song_id", songId)
         .single();
 
+      // if there is no error, and there is a match, set the isLiked to true
       if (!error && data) {
         setIsLiked(true);
       }
@@ -40,7 +43,50 @@ const LikeButton: React.FC<LikeButtonProps> = ({ songId }) => {
     fetchData();
   }, [songId, supabaseClient, user?.id]);
 
-  return <div>Like Button</div>;
+  const Icon = isLiked ? AiFillHeart : AiOutlineHeart;
+
+  // handle like and unlike
+  const handleLike = async () => {
+    // if the user has not logged in, ask them to log in
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    // if the user has already liked the song, remove it from the supabase liked_songs table and set isLiked to false
+    if (isLiked) {
+      const { error } = await supabaseClient
+        .from("liked_songs")
+        .delete()
+        .eq("user_id", user.id)
+        .eq("song_id", songId);
+
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(false);
+      }
+    } else {
+      // if the user has not liked the song, insert it to the supabase liked_songs table and set isLike to true
+      const { error } = await supabaseClient
+        .from("liked_songs")
+        .insert({ song_id: songId, user_id: user.id });
+
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(true);
+        toast.success("Liked!");
+      }
+    }
+
+    router.refresh();
+  };
+
+  return (
+    <button onClick={handleLike} className="hover:opacity-75 transition">
+      <Icon color={isLiked ? "#22c55e" : "#white"} size={25} />
+    </button>
+  );
 };
 
 export default LikeButton;
