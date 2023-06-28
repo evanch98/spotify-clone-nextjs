f9721bdba593fc5f19a0d4bb902a24513bffc1ac
Kyaw Thu <evanch98@gmail.com>
2023-06-28 18:55:05 +0800

    add a new component SearchContent

---
+++ app/search/components/SearchContent.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Song } from "@/types";
+
+interface SearchContentProps {
+  songs: Song;
+}
+
+const SearchContent: React.FC<SearchContentProps> = ({songs}) => {
+  return <div>Search Content</div>;
+};
+
+export default SearchContent;
