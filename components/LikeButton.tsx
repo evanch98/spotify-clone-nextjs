dcc2f4060442e3187015e9a6af6a9264460f5b43
Kyaw Thu <evanch98@gmail.com>
2023-06-28 19:29:10 +0800

    update the LikeButton component

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,10 +1,45 @@
 "use client";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import { useSessionContext } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 interface LikeButtonProps {
   songId: string;
 }
 
 const LikeButton: React.FC<LikeButtonProps> = ({ songId }) => {
+  const router = useRouter();
+  const { supabaseClient } = useSessionContext();
+  const authModal = useAuthModal();
+  const { user } = useUser();
+  const [isLiked, setIsLiked] = useState(false);
+
+  // to check whether the song has been liked or not
+  useEffect(() => {
+    if (!user?.id) {
+      return;
+    }
+
+    // fetch data from the liked_songs table where user_id = user.id (current user) and song_id = songId
+    const fetchData = async () => {
+      const { data, error } = await supabaseClient
+        .from("liked_songs")
+        .select("*")
+        .eq("user_id", user.id)
+        .eq("song_id", songId)
+        .single();
+
+      if (!error && data) {
+        setIsLiked(true);
+      }
+    };
+
+    fetchData();
+  }, [songId, supabaseClient, user?.id]);
+
   return <div>Like Button</div>;
 };
 
