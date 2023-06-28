1d14a36d9f27662a9ff9bc7415a4788ffd4570a4
Kyaw Thu <evanch98@gmail.com>
2023-06-28 17:38:26 +0800

    add the SearchInput component

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,6 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from "@/components/Header";
+import SearchInput from "@/components/SearchInput";
 
 interface SearchProps {
   searchParams: {
@@ -16,6 +17,7 @@ const Search = async ({ searchParams }: SearchProps) => {
       <Header className="from-bg-neutral-900">
         <div className="mb-2 flex flex-col gap-y-6">
           <h1 className="text-white text-3xl font-semibold">Search</h1>
+          <SearchInput />
         </div>
       </Header>
     </div>
