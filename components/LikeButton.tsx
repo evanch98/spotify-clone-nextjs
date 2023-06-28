68d8d333a563566659e98c17acc6b21707e2a926
Kyaw Thu <evanch98@gmail.com>
2023-06-28 19:20:10 +0800

    add a new component LikeButton

---
+++ components/LikeButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+interface LikeButtonProps {
+  songId: string;
+}
+
+const LikeButton: React.FC<LikeButtonProps> = ({ songId }) => {
+  return <div>Like Button</div>;
+};
+
+export default LikeButton;
