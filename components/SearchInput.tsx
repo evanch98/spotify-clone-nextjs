b170a2910883cc59ad437003daf1ba7ec4cc5afd
Kyaw Thu <evanch98@gmail.com>
2023-06-28 18:52:43 +0800

    update the SearchInput component

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,14 +1,33 @@
 "use client";
 
+import qs from "query-string";
 import useDebounce from "@/hooks/useDebounce";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Input from "./Input";
 
 const SearchInput = () => {
   const router = useRouter();
   const [value, setValue] = useState<string>("");
-  const debouncedValue = useDebounce<string>(value, 500);
-  return <div>Search Input</div>;
+  const debouncedValue = useDebounce<string>(value, 500); // to make sure the value is load to the url only after 500 milliseconds
+
+  useEffect(() => {
+    const query = { title: debouncedValue };
+    // construct a url (e.g. /search/debouncedValue)
+    const url = qs.stringifyUrl({
+      url: "/search",
+      query: query,
+    });
+
+    router.push(url);
+  }, [debouncedValue, router]);
+  return (
+    <Input
+      placeholder="What do you want to listen to?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 };
 
 export default SearchInput;
