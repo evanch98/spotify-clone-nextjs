6e08a1236bdd1a51ce0cb7b37c9ae09dd629c864
Kyaw Thu <evanch98@gmail.com>
2023-06-29 13:11:18 +0800

    import the Player component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
 import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/components/Player";
 
 const font = Figtree({ subsets: ["latin"] });
 
