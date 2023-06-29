7206bfded262b27375f79a61ce37d35e4180d464
Kyaw Thu <evanch98@gmail.com>
2023-06-29 13:08:05 +0800

    add a new hook usePlayer

--- hooks/usePlayer.ts
+++ hooks/usePlayer.ts
@@ -1,9 +1,19 @@
+import { create } from "zustand";
+
 interface PlayerStore {
-  ids: string[];
-  activeId?: string;
-  setId: (id: string) => void;
-  setIds: (ids: string) => void;
-  reset: () => void;
+  ids: string[]; // a list of song ids to be played
+  activeId?: string; // the id of the currently playing song
+  setId: (id: string) => void; // set the activeId
+  setIds: (ids: string[]) => void; // set the ids
+  reset: () => void; // reset
 }
 
+const usePlayer = create<PlayerStore>((set) => ({
+  ids: [],
+  activeId: undefined,
+  setId: (id: string) => set({ activeId: id }),
+  setIds: (ids: string[]) => set({ ids: ids }),
+  reset: () => set({ ids: [], activeId: undefined }),
+}));
 
+export default usePlayer;
