f55fe846e04aba8611da1b123bd1590f7464719c
Kyaw Thu <evanch98@gmail.com>
2023-06-29 13:23:01 +0800

    update the Player component

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import useGetSongById from "@/hooks/useGetSongById";
 import usePlayer from "@/hooks/usePlayer";
 
 const Player = () => {
   const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
   return <div>Player</div>;
 };
 
