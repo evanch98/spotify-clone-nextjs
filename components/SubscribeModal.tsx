ef4964dd5a8ccf90957a63ba77052f332ddf15c6
Kyaw Thu <evanch98@gmail.com>
2023-07-01 02:33:40 +0800

    update the SubscribeModal component

--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import Modal from "./Modal";
+
 const SubscribeModal = () => {
-  return <div>Subscribe Modal</div>;
+  return (
+    <Modal
+      title="Only for premium users"
+      description="Listen to music with Spotify Premium"
+      isOpen
+      onChange={() => {}}
+    >
+      Subscription
+    </Modal>
+  );
 };
 
 export default SubscribeModal;
