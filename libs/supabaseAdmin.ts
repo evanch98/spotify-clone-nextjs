9b632c010278d5a854b11291cd702b6c708856de
Kyaw Thu <evanch98@gmail.com>
2023-06-30 23:49:43 +0800

    add supabaseAdmin and a new function upsertProductRecord

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -4,3 +4,29 @@ import { Database } from "@/types_db";
 import { Price, Product } from "@/types";
 import { stripe } from "./stripe";
 import { toDateTime } from "./helpers";
+
+// create a new Supabase client
+export const supabaseAdmin = createClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ""
+);
+
+// to upsert the product data to the Supabase when there is a new product on the Stripe
+const upsertProductRecord = async (product: Stripe.Product) => {
+  const productData: Product = {
+    id: product.id,
+    active: product.active,
+    name: product.name,
+    description: product.description ?? undefined,
+    image: product.images?.[0] ?? null,
+    metadata: product.metadata,
+  };
+
+  const { error } = await supabaseAdmin.from("products").upsert([productData]);
+
+  if (error) {
+    throw error;
+  }
+
+  console.log(`Product inserted/updated: ${product.id}`);
+};
