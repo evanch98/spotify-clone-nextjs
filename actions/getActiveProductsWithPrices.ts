e927a9e73fb40c305f3e0002dd85072de46c868c
Kyaw Thu <evanch98@gmail.com>
2023-07-01 02:39:34 +0800

    initialize the getActiveProductsWithPrices

---
+++ actions/getActiveProductsWithPrices.ts
@@ -0,0 +1,26 @@
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+// to fetch the songs from the server
+// the function will return the Promise<Song[]>
+const getActiveProductsWithPrices = async (): Promise<Song[]> => {
+  // create the server component client
+  const supabase = createServerComponentClient({
+    cookies: cookies,
+  });
+
+  // fetch the data from the songs table
+  const { data, error } = await supabase
+    .from("songs")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.log(error);
+  }
+
+  return (data as any) || [];
+};
+
+export default getActiveProductsWithPrices;
