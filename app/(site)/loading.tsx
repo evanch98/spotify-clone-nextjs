51e7ab0d981354a61728899be0aed79bfedd1a06
Kyaw Thu <evanch98@gmail.com>
2023-06-30 22:02:25 +0800

    update the Loading component

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import Box from "@/components/Box";
+import { BounceLoader } from "react-spinners";
 
 const Loading = () => {
   return (
-    <Box className="h-full flex items-center justify-center">Loading...</Box>
+    <Box className="h-full flex items-center justify-center">
+      <BounceLoader color="#22c55e" size={40} />
+    </Box>
   );
 };
 
