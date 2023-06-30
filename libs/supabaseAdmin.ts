9b00b98f3fca169c51698a37d8affa45bb630e18
Kyaw Thu <evanch98@gmail.com>
2023-06-30 23:55:20 +0800

    add a new function upsertPriceRecord

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -30,3 +30,28 @@ const upsertProductRecord = async (product: Stripe.Product) => {
 
   console.log(`Product inserted/updated: ${product.id}`);
 };
+
+// to upsert the price data to the Supabase when there is a new price on the Stripe
+const upsertPriceRecord = async (price: Stripe.Price) => {
+  const priceData: Price = {
+    id: price.id,
+    product_id: typeof price.product === "string" ? price.product : "",
+    active: price.active,
+    currency: price.currency,
+    description: price.nickname ?? undefined,
+    type: price.type,
+    unit_amount: price.unit_amount ?? undefined,
+    interval: price.recurring?.interval,
+    interval_count: price.recurring?.interval_count,
+    trial_period_days: price.recurring?.trial_period_days,
+    metadata: price.metadata,
+  };
+
+  const { error } = await supabaseAdmin.from("prices").upsert([priceData]);
+
+  if (error) {
+    throw error;
+  }
+
+  console.log(`Price inserted/updated: ${price.id}`);
+};
