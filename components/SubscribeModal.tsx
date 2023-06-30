e71bf4bfea5d52bdc2784f6d38e008216e7ea242
Kyaw Thu <evanch98@gmail.com>
2023-07-01 03:04:24 +0800

    update the SubscribeModal component

--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,14 +1,65 @@
 "use client";
 
-import { ProductWithPrice } from "@/types";
+import { Price, ProductWithPrice } from "@/types";
 import Modal from "./Modal";
+import Button from "./Button";
+import { useState } from "react";
+import { useUser } from "@/hooks/useUser";
+import { toast } from "react-hot-toast";
 
 interface SubscribeModalProps {
   products: ProductWithPrice[];
 }
 
+const formatPrice = (price: Price) => {
+  const priceString = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: price.currency,
+    maximumFractionDigits: 0,
+  }).format((price?.unit_amount || 0) / 100);
+  return priceString;
+};
+
 const SubscribeModal: React.FC<SubscribeModalProps> = ({ products }) => {
+  const [priceIdLoading, setPriceIdLoading] = useState<string>();
+  const { user, isLoading, subscription } = useUser();
+  const handleCheckout = async (price: Price) => {
+    setPriceIdLoading(price.id);
+
+    if (!user) {
+      setPriceIdLoading(undefined);
+      return toast.error("Must be logged in");
+    }
+
+    if (subscription) {
+      setPriceIdLoading(undefined);
+      return toast("Already subscribed");
+    }
+  };
   let content = <div className="text-center">No products available</div>;
+
+  if (products.length) {
+    content = (
+      <div>
+        {products.map((product) => {
+          if (!product.prices?.length) {
+            return <div key={product.id}>No prices available</div>;
+          }
+
+          return product.prices.map((price) => (
+            <Button
+              key={price.id}
+              onClick={() => handleCheckout(price)}
+              disabled={isLoading || price.id === priceIdLoading}
+              className="mb-4"
+            >
+              {`Subscribe for ${formatPrice(price)} a ${price.interval}`}
+            </Button>
+          ));
+        })}
+      </div>
+    );
+  }
   return (
     <Modal
       title="Only for premium users"
