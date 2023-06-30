7c8cf05a272811a5d04ec3544edbdea60adb5f50
Kyaw Thu <evanch98@gmail.com>
2023-06-30 23:05:23 +0800

    add a new function toDateTime

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -37,3 +37,9 @@ export const postData = async ({
 
   return response.json();
 };
+
+export const toDateTime = (secs: number) => {
+  var t = new Date("1970-01-01T00:30:00Z"); // Unix epoch start
+  t.setSeconds(secs);
+  return t;
+};
