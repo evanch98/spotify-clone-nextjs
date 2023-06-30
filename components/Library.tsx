20c28c5720312d242ea7aeccc6af0ac1869e885a
Kyaw Thu <evanch98@gmail.com>
2023-06-30 17:54:33 +0800

    update the Library component

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,6 +7,7 @@ import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LibraryProps {
   songs: Song[];
@@ -16,6 +17,8 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
+  const onPlay = useOnPlay(songs);
+
   const onClick = () => {
     // if the user has not logged in, ask them to log in
     if (!user) {
@@ -42,7 +45,7 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
         {songs.map((song) => (
-          <MediaItem key={song.id} data={song} onClick={() => {}} />
+          <MediaItem key={song.id} data={song} onClick={(id: string) => onPlay(id)} />
         ))}
       </div>
     </div>
