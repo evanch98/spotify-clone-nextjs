8aa777f7623ea212062578374baa6e05a4f69e81
Kyaw Thu <evanch98@gmail.com>
2023-06-30 17:16:05 +0800

    update the PlayerContent component

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -8,7 +8,8 @@ import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 import Slider from "./Slider";
 import usePlayer from "@/hooks/usePlayer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
 
 interface PlayerContentProps {
   song: Song;
@@ -19,10 +20,53 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
   const player = usePlayer();
   const [volume, setVolume] = useState(1);
   const [isPlaying, setIsPlaying] = useState(false);
+
+  // this will provide play, pause, and sound
+  // the song that will be played is provided by songUrl (from the supabase storage)
+  const [play, { pause, sound }] = useSound(songUrl, {
+    volume: volume,
+    onplay: () => setIsPlaying(true),
+    onend: () => {
+      setIsPlaying(false);
+      onPlayNext();
+    },
+    onpause: () => setIsPlaying(false),
+    format: ["mp3"], // the format of the sound should be mp3 (for this project)
+  });
+
+  // to play the sound automatically when the PlayerContent component is loaded
+  useEffect(() => {
+    sound?.play();
+
+    return () => {
+      sound?.unload();
+    };
+  }, [sound]);
+
   // if isPlaying is true, show the pause icon; otherwise, show the play icon
   const Icon = isPlaying ? BsPauseFill : BsPlayFill;
   // if the volume is zero show the speaker icon with a x mark; otherwise, show the speaker icon with a wave
   const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+  // handle the play button's function
+  const handlePlay = () => {
+    // if the song is not playing, play the song; otherwise, pause it
+    if (!isPlaying) {
+      play();
+    } else {
+      pause();
+    }
+  };
+
+  // to set the volume to mute and unmute
+  const toggleMute = () => {
+    // if the volume is already muted (volume === 0), unmute it; otherwise, mute it
+    if (volume === 0) {
+      setVolume(1);
+    } else {
+      setVolume(0);
+    }
+  };
 
   // handle the next button's function
   const onPlayNext = () => {
@@ -101,7 +145,11 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
       </div>
       <div className="hidden md:flex w-full justify-end pr-2">
         <div className="flex items-center gap-x-2 w-[120px]">
-          <VolumeIcon onClick={() => {}} className="cursor-pointer" size={34} />
+          <VolumeIcon
+            onClick={toggleMute}
+            className="cursor-pointer"
+            size={34}
+          />
           <Slider />
         </div>
       </div>
