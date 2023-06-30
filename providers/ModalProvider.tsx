e266c7e56da5c3ef47858be6248b4c22f1a9e97d
Kyaw Thu <evanch98@gmail.com>
2023-07-01 02:34:25 +0800

    add the SubscribeModal component

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import AuthModal from "@/components/AuthModal";
+import SubscribeModal from "@/components/SubscribeModal";
 import UploadModal from "@/components/UploadModal";
 import { useEffect, useState } from "react";
 
@@ -20,6 +21,7 @@ const ModalProvider = () => {
     <>
       <AuthModal />
       <UploadModal />
+      <SubscribeModal />
     </>
   );
 };
