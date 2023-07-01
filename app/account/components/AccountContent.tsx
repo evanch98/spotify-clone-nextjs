64b917d42852eb279c72d30042215230f7a2fd19
Kyaw Thu <evanch98@gmail.com>
2023-07-01 12:02:09 +0800

    update the AccountContent component

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -1,7 +1,54 @@
 "use client";
 
+import Button from "@/components/Button";
+import useSubscribeModal from "@/hooks/useSubscribeModal";
+import { useUser } from "@/hooks/useUser";
+import { postData } from "@/libs/helpers";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+
 const AccountContent = () => {
-  return <div>Account Content</div>;
+  const router = useRouter();
+  const subscribeModal = useSubscribeModal();
+  const { isLoading, subscription, user } = useUser();
+
+  const [loading, setLoading] = useState(false);
+
+  // if the user has not logged in, do not allow them to enter this page
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+
+  const redirectToCustomerPortal = async () => {
+    setLoading(true);
+    try {
+      const { url, error } = await postData({
+        url: "/api/create-portal-link",
+      });
+      window.location.assign(url);
+    } catch (error) {
+      if (error) {
+        toast.error((error as Error).message);
+      }
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="mb-7 px-6">
+      {!subscription && (
+        <div className="flex flex-col gap-y-4">
+          <p>No active plan.</p>
+          <Button onClick={subscribeModal.onOpen} className="w-[300px]">
+            Subscribe
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AccountContent;
