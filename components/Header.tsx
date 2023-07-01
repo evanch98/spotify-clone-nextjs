5656d03a1b57fd73d1bbb1af2a16de409a575c97
Kyaw Thu <evanch98@gmail.com>
2023-07-01 11:38:34 +0800

    update the Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,6 +11,7 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "@/hooks/useUser";
 import { FaUserAlt } from "react-icons/fa";
 import { toast } from "react-hot-toast";
+import usePlayer from "@/hooks/usePlayer";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -20,6 +21,7 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
   const authModal = useAuthModal();
   const router = useRouter();
+  const player = usePlayer();
 
   const supabaseClient = useSupabaseClient();
   const { user } = useUser();
@@ -27,7 +29,8 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
   const handleLogout = async () => {
     // Handle log out
     const { error } = await supabaseClient.auth.signOut();
-    // TODO: Reset any playing songs
+    // Reset any playing songs
+    player.reset();
     router.refresh();
 
     if (error) {
