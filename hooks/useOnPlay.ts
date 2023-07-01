e87d14d90da5d5fae95c7052eec4b91f3f7ab7dd
Kyaw Thu <evanch98@gmail.com>
2023-07-01 11:35:43 +0800

    update the Library component

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -2,17 +2,24 @@ import { Song } from "@/types";
 import usePlayer from "./usePlayer";
 import useAuthModal from "./useAuthModal";
 import { useUser } from "./useUser";
+import useSubscribeModal from "./useSubscribeModal";
 
 // when the user clicks the play button, use this hook to set the ids of the songs to be played
 const useOnPlay = (songs: Song[]) => {
+  const subscribeModal = useSubscribeModal();
   const player = usePlayer();
   const authModal = useAuthModal();
-  const { user } = useUser();
+  const { user, subscription } = useUser();
 
   const onPlay = (id: string) => {
     // if the user has not logged in, ask them to log in
     if (!user) {
       return authModal.onOpen();
+    }
+
+    // restrict the user from playing songs if they have not subscribed the premium service
+    if (!subscription) {
+      return subscribeModal.onOpen();
     }
 
     // set the current song to be played
