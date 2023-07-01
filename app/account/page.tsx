6963aa26f490f4d38029c9e32796f8273f92edf1
Kyaw Thu <evanch98@gmail.com>
2023-07-01 11:48:52 +0800

    update the Account component

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,5 +1,17 @@
+import Header from "@/components/Header";
+
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header className="from-bg-neutral-900">
+        <div className="mb-2 flex flex-col gap-y-6">
+          <h1 className="text-white text-3xl font-semibold">
+            Account Settings
+          </h1>
+        </div>
+      </Header>
+    </div>
+  );
 };
 
 export default Account;
